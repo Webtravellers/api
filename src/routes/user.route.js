8a228a0ba2baef729fd547bdaec205a608bf9859
yasintorun <yasintorun4680@gmail.com>
2022-03-27 12:54:11 +0300

    Added base model

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,7 +4,7 @@ import { addUser, getUsers } from "../controllers/user.controller.js";
 const userRouter = new express.Router()
 
 userRouter.get("/", getUsers)
-userRouter.post("/asd", addUser)
+userRouter.post("/", addUser)
 
 
 export default userRouter
