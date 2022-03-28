c82c76633c638e884b2291afd8263910f01a8ad1
yasintorun <yasintorun4680@gmail.com>
2022-03-28 22:54:38 +0300

    Edit comment model

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -1,16 +1,18 @@
 import mongoose from "mongoose"
 
 const commentSchema = new mongoose.Schema({
-    userId: {
-        type: Number,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
         require: true
     },
     comment: {
         type: String,
         require: true
     },
-    locationId: {
-        type: Number,
+    location: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "locations",
         require: true
     }
 })
