427add2bc3387c40048af1ae404d00c4b7e8b2e0
AlifurkanKaraman <82064494+AlifurkanKaraman@users.noreply.github.com>
2022-03-29 19:28:28 +0300

    Created photo-types

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,6 +3,7 @@ import userRouter from "./user.route.js";
 import locationRouter from "./location.route.js";
 import cityRouter from "./city.route.js";
 import locationTypeRouter from "./location-types.route.js";
+import photoTypeRouter from "./photo-types.route.js";
 
 const router = express.Router();
 
@@ -16,5 +17,6 @@ router.use("/users", userRouter)
 router.use("/locations", locationRouter)
 router.use("/cities", cityRouter)
 router.use("/location-types", locationTypeRouter)
+router.use("/photo-types", photoTypeRouter)
 
 export default router;
