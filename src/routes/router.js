ccd655e344ad02acafcac576bd9bc878c5f08926
yasintorun <yasintorun4680@gmail.com>
2022-04-03 15:31:20 +0300

    Update router.js

--- src/routes/router.js
+++ src/routes/router.js
@@ -5,8 +5,6 @@ import commentRouter from "./comment.route.js";
 import listsRouter from "./lists.route.js";
 import cityRouter from "./city.route.js";
 import locationTypeRouter from "./location-types.route.js";
-import photoTypeRouter from "./photo-types.route.js";
-import photoRouter from "./photos.route.js";
 
 const router = express.Router();
 
@@ -21,7 +19,5 @@ router.use("/comments", commentRouter)
 router.use("/lists", listsRouter)
 router.use("/cities", cityRouter)
 router.use("/location-types", locationTypeRouter)
-router.use("/photo-types", photoTypeRouter)
-router.use("/photo",photoRouter)
 
 export default router;
