93f588b9f94b59d3497c97bdb82a2d0520a4e1b7
AlifurkanKaraman <82064494+AlifurkanKaraman@users.noreply.github.com>
2022-04-03 15:21:09 +0300

    Deleted comment model
    Comment ile ilgili her şey silindi (Comment model, route, controller)

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,7 +1,6 @@
 import express from "express"
 import userRouter from "./user.route.js";
 import locationRouter from "./location.route.js";
-import commentRouter from "./comment.route.js";
 import listsRouter from "./lists.route.js";
 import cityRouter from "./city.route.js";
 import locationTypeRouter from "./location-types.route.js";
@@ -16,7 +15,6 @@ router.get("/", (req, res, next) => {
 })
 router.use("/users", userRouter)
 router.use("/locations", locationRouter)
-router.use("/comments", commentRouter)
 router.use("/lists", listsRouter)
 router.use("/cities", cityRouter)
 router.use("/location-types", locationTypeRouter)
