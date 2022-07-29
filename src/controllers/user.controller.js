e5e054825008e9acd3b97eb8a2434138d4ffe9c3
AlifurkanKaraman <82064494+AlifurkanKaraman@users.noreply.github.com>
2022-07-29 23:53:39 +0300

    Edit Profile added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,39 +1,62 @@
 import UserModel from "../models/user.model.js";
 import Result from "../utils/Result.js";
+import asyncHandler from "express-async-handler";
+import { createToken } from "./auth.controller.js";
 
 const addUser = async (req, res, next) => {
-    const user = req.body
-    try {
-        await new UserModel(user).save()
-        Result.success(res, "Kaydedildi")
-    } catch (err) {
-        next(err)
-    }
-}
-
-
+  const user = req.body;
+  try {
+    await new UserModel(user).save();
+    Result.success(res, "Kaydedildi");
+  } catch (err) {
+    next(err);
+  }
+};
 
 const getUsers = async (req, res, next) => {
-    try {
-        const users = await UserModel.find()
-        Result.success(res, "user.listed", users)
-    } catch (err) {
-        next(err)
-    }
-}
+  try {
+    const users = await UserModel.find();
+    Result.success(res, "user.listed", users);
+  } catch (err) {
+    next(err);
+  }
+};
 
 const getUserById = async (req, res, next) => {
-    const userId = req.params.id
-    try {
-        const user = await UserModel.findById(String(userId))
-        Result.success(res, `User with the ID ${userId}`, user)
-    } catch (error) {
-        next(error)
-    }
-}
+  const userId = req.params.id;
+  try {
+    const user = await UserModel.findById(String(userId));
+    Result.success(res, `User with the ID ${userId}`, user);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export {
-    addUser,
-    getUsers,
-    getUserById
-}
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await UserModel.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.photo = req.body.photo || user.photo;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      photo: updatedUser.photo,
+      token: createToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
+});
+
+export { addUser, getUsers, getUserById, updateUserProfile };
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,12 +1,23 @@
 import express from "express";
-import { addUser, getUserById, getUsers } from "../controllers/user.controller.js";
-import { signIn, signUp, sendResetLink, resetPassword } from "../controllers/auth.controller.js";
+import {
+  addUser,
+  getUserById,
+  getUsers,
+  updateUserProfile,
+} from "../controllers/user.controller.js";
+import {
+  signIn,
+  signUp,
+  sendResetLink,
+  resetPassword,
+} from "../controllers/auth.controller.js";
+import { AuthenticationMiddleware } from "../middleware/authenticationMiddleware.js";
 
-const userRouter = new express.Router()
+const userRouter = new express.Router();
 
-userRouter.get("/", getUsers)
-userRouter.post("/", addUser)
-userRouter.get('/:id', getUserById)
+userRouter.get("/", getUsers);
+userRouter.post("/", addUser);
+userRouter.get("/:id", getUserById);
+userRouter.route("/update").post(AuthenticationMiddleware, updateUserProfile);
 
-
-export default userRouter
+export default userRouter;
